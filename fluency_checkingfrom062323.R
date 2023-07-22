55f845a12fb7f25e7bf5a680c781c007d67973f0
REBECCA Anne WILDER <71036276+rawilder95@users.noreply.github.com>
2023-07-22 10:12:12 -0400

    cluster switches
    got cluster switches from online tool

--- fluency_checkingfrom062323.R
+++ fluency_checkingfrom062323.R
@@ -2,17 +2,11 @@ rm(list= ls())
 library(data.table)
 library(ggplot2)
 library(gridExtra)
-# library(ezANOVA)
+library(ez)
 library(cowplot)
-<<<<<<< Updated upstream
 source('bbar.r')
-setwd('~/Downloads/jatos_mac_java/analyses')
+#setwd('~/Downloads/jatos_mac_java/analyses') # JCZ
 
-=======
-setwd('~/Downloads/jatos_mac_java/analyses')
-# source('bbar.r')
-# d38afbc4ffb1314100d63cda757d9dbfd023f925
->>>>>>> Stashed changes
 # load data
 d1 <- fread('cleanfluency.csv')
 d1[, listnum:= gamenum]
@@ -43,15 +37,8 @@ dat[ageText>=60, age:= "old"]
 # get rid of participants that violated age requirements
 dat <- dat[!(age == "WRONG AGE"),]
 
-# @Rebecca these participants did not start naming animals for >30s, grounds for exclusion?
-dat<-dat[!(prolific_id=="5b33a01fa8327d0001003821" | prolific_id=="5b33a01fa8327d0001003821")]
-
-# @Rebecca in calculating the one_trialers (below), i noticed all of the participants who are removed have List 2 but *not* List 1, which doesn't make sense.
-# I checked the starttime and noticed that the list numbers are reversed for all participants in the Delayed condition
-# Please fix in the original data file and then delete the three lines below
-dat[condition=="Delayed" & listnum == 2, listnum := 3]  # these three lines swap listnums for participants in the delayed condition
-dat[condition=="Delayed" & listnum == 1, listnum := 2]
-dat[condition=="Delayed" & listnum == 3, listnum := 1]
+# these participants did not start naming animals for >30s, grounds for exclusion?
+#dat<-dat[!(prolific_id=="5b33a01fa8327d0001003821" | prolific_id=="5b33a01fa8327d0001003821")]
 
 # Get rid of people who only did one trial; Including Immediate
 one_trialers= dat[, unique(listnum), by= prolific_id][, sum(V1), by= prolific_id][V1<3,prolific_id]
@@ -100,9 +87,9 @@ dat[listnum==2, proportion_repeat := sum(is_repeat)/.N, by= .(prolific_id)]
 # Q: does proportion of repeats differ by age or condition?
 dat_repeat <- unique(dat[listnum==2,.(prolific_id, age, condition, proportion_repeat)]) # new data table with 1 row per participant (2nd list only)
 
-# @Rebecca 7 participants did not repeat any items, and 6 of these are from the Immediate condition. it seems likely that they misunderstood the instructions
+# 7 participants did not repeat any items, and 6 of these are from the Immediate condition. it seems likely that they misunderstood the instructions
 # maybe we should not inlcude these participants? 
-dat_repeat<-dat_repeat[proportion_repeat != 0.0] 
+#dat_repeat<-dat_repeat[proportion_repeat != 0.0] 
 
 dat_repeat[,mean(proportion_repeat), by=.(age,condition)] # raw numbers
 ezANOVA(dat_repeat, between=c("age","condition"), dv=proportion_repeat, wid=prolific_id)
@@ -116,43 +103,43 @@ bbar(dat_repeat[, proportion_repeat, by=.(age, condition)])
 
 ### CHECK ROWS END HERE ###
 # Repetitions
-toplot <- dat[, .N, by= .(items, prolific_id, age, condition)][, sum(N>1)/length(N), by= .(age, condition)]
-ggplot(data= toplot)+ geom_bar(aes(x= age ,y= V1, fill= condition), stat= "identity", position= "dodge")+ labs(x= "Age Group", y= "N Items Repeated", fill= "Task Condition")
-ggsave("nrepeated_bar.png", device= 'png', dpi= 300)
+#toplot <- dat[, .N, by= .(items, prolific_id, age, condition)][, sum(N>1)/length(N), by= .(age, condition)]
+#ggplot(data= toplot)+ geom_bar(aes(x= age ,y= V1, fill= condition), stat= "identity", position= "dodge")+ labs(x= "Age Group", y= "N Items Repeated", fill= "Task Condition")
+#ggsave("figs/nrepeated_bar.png", device= 'png', dpi= 300)
 
 
 
 # repeat repeat trnaitions 
-dat[, N:= .N, by= .(items, prolific_id, age, condition)]
-dat[, repeated:= as.numeric(N>1)] #get indices for repeated vals
-rr_idx= which(dat[, diff(repeated)]==-1) #index all of the rows that are a repeat repeat transition, i.e. 1-1 should == 0
-
-rr_trials= dat[rr_idx,]
-p_rrtrials= rr_trials[, .N/nrow(rr_trials), by= .(condition, age)]
-ggplot(p_rrtrials, aes(x= age, y= V1, fill= condition))+ geom_bar(stat= "identity", position= "dodge")+ theme_classic()+ labs(x= "Age Group", y= "Proportion of Trials", title= "Repeat-Repeat Transitions", fill= "Trial Condition")
-ggsave("prepeatedrepeat_bar.png", device= 'png', dpi= 300)
-
-dat[, listnum := factor(listnum)]
-dat[, age := factor(age)]
-dat[, condition := factor(condition)]
-dat[, prolific_id := factor(prolific_id)]
-# 14 people with no list 2
-bad_ids <- dat[,.N,by=.(prolific_id,listnum)][,.N,by=prolific_id][N==1][,prolific_id]
-dat <- dat[!(prolific_id %in% bad_ids)]
-# 1 person with multiple rows per response (why?)
-dat <- unique(dat)
-dat[, N:=.N, by=.(prolific_id, listnum, items)][N>1,items:= NaN, by= .(prolific_id, listnum, items)]
-dat[N>1 & !(itemnum== min(itemnum)), items:= "PERSEVERATION", by= .(prolific_id, listnum,items)]
-dat_n <- dat[,.N,by=.(prolific_id,age,listnum,condition)]
-plotdat <- dat_n[,.(N=mean(N)),by=.(age,condition,listnum)]
-
-a <- ggplot(plotdat[condition=="Delayed"], aes(y=N, x=age, group=listnum, fill=listnum)) + geom_bar(stat="identity",position="dodge") + ylim(0,30) + ggtitle("Delayed")
-b <- ggplot(plotdat[condition=="Immediate"], aes(y=N, x=age, group=listnum, fill=listnum)) + geom_bar(stat="identity",position="dodge") + ylim(0,30) + ggtitle("Immediate")
-grid.arrange(a,b)
-
-ezANOVA(dat_n, within=listnum, between=c("age","condition"), dv=N, wid=prolific_id)
-
-
-
-dat= dat[prolific_id %in% dat[, .N, by= .(prolific_id, listnum)][, .N, by= prolific_id][N==2,prolific_id],]
+#dat[, N:= .N, by= .(items, prolific_id, age, condition)]
+#dat[, repeated:= as.numeric(N>1)] #get indices for repeated vals
+#rr_idx= which(dat[, diff(repeated)]==-1) #index all of the rows that are a repeat repeat transition, i.e. 1-1 should == 0
+#
+#rr_trials= dat[rr_idx,]
+#p_rrtrials= rr_trials[, .N/nrow(rr_trials), by= .(condition, age)]
+#ggplot(p_rrtrials, aes(x= age, y= V1, fill= condition))+ geom_bar(stat= "identity", position= "dodge")+ theme_classic()+ labs(x= "Age Group", y= "Proportion of Trials", title= "Repeat-Repeat Transitions", fill= "Trial Condition")
+#ggsave("figs/prepeatedrepeat_bar.png", device= 'png', dpi= 300)
+#
+#dat[, listnum := factor(listnum)]
+#dat[, age := factor(age)]
+#dat[, condition := factor(condition)]
+#dat[, prolific_id := factor(prolific_id)]
+## 14 people with no list 2
+#bad_ids <- dat[,.N,by=.(prolific_id,listnum)][,.N,by=prolific_id][N==1][,prolific_id]
+#dat <- dat[!(prolific_id %in% bad_ids)]
+## 1 person with multiple rows per response (why?)
+#dat <- unique(dat)
+#dat[, N:=.N, by=.(prolific_id, listnum, items)][N>1,items:= NaN, by= .(prolific_id, listnum, items)]
+#dat[N>1 & !(itemnum== min(itemnum)), items:= "PERSEVERATION", by= .(prolific_id, listnum,items)]
+#dat_n <- dat[,.N,by=.(prolific_id,age,listnum,condition)]
+#plotdat <- dat_n[,.(N=mean(N)),by=.(age,condition,listnum)]
+#
+#a <- ggplot(plotdat[condition=="Delayed"], aes(y=N, x=age, group=listnum, fill=listnum)) + geom_bar(stat="identity",position="dodge") + ylim(0,30) + ggtitle("Delayed")
+#b <- ggplot(plotdat[condition=="Immediate"], aes(y=N, x=age, group=listnum, fill=listnum)) + geom_bar(stat="identity",position="dodge") + ylim(0,30) + ggtitle("Immediate")
+#grid.arrange(a,b)
+#
+#ezANOVA(dat_n, within=listnum, between=c("age","condition"), dv=N, wid=prolific_id)
+#
+#
+#
+#dat= dat[prolific_id %in% dat[, .N, by= .(prolific_id, listnum)][, .N, by= prolific_id][N==2,prolific_id],]
 fwrite(dat, 'fluency_data_07152023.csv')
