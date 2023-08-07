61138515dad1aff9f20b626b0706c74543440f5b
REBECCA Anne WILDER <71036276+rawilder95@users.noreply.github.com>
2023-08-07 10:22:14 -0400

    Getting rid of redundancies

--- perseverationcode.R
+++ perseverationcode.R
@@ -15,28 +15,26 @@ dat= fread('combinedfluency.csv')
 dat[, word_counts:=.N, by= .(prolific_id, condition, listnum, items)]
 # make a perseverations col
 dat[, perseveration:= 0]
+# Here we index *all* instances where an item was listed more than once per trial.  On the next line we exclude cases where it was the first time an item was listed
 dat[word_counts>1, perseveration:=1]
-# do bar graph
+# exclude the first time the item was generated (smallest itemnum) because this isn't a perseveration
+dat[perseveration== 1 & itemnum== min(itemnum), perseveration:=0, by= .(prolific_id,items)]
+#Bar graph for perseveration rate
 toplot1= dat[listnum==1, mean(perseveration), by= .(listnum,condition, age)]
 toplot2= dat[listnum==2, mean(perseveration), by= .(listnum,condition, age)]
 p1= ggplot(data= toplot1, aes(x= age, y= V1, fill= condition))+ geom_bar(stat= 'identity', position= 'dodge')+ labs(x= 'Age', y= 'Perseveration Rate', fill= "Condition", title= "Perseverative Errors Trial 1")+ theme_classic()
 p2= ggplot(data= toplot2, aes(x= age, y= V1, fill= condition))+ geom_bar(stat= 'identity', position= 'dodge')+ labs(x= 'Age', y= 'Perseveration Rate', fill= "Condition", title= "Perseverative Errors Trial 2")+theme_classic()
 cowplot::plot_grid(p1,p2)
-
-# which items were said more than once on any particular trial
+ggsave('perseverationsbargraph.png', device= 'png', dpi= 300)
+# Write data file with perseverations
+# create a variable for perseverations to keep everything compartmentalized
+# which items were said more than once on any particular trial?
 # *perseverations* = data.table
 # *perseveration* = col identifier in dat data.table
-perseverations= dat
-# Now get rid of perseverations in dat (itemnum will mark still mark the ordinal positions)
-dat[perseveration== 0]
-# We set all words that were repeated more than once to 1 *initially* (before going back and setting the smallest itemnum to 0)
-perseverations[perseveration== 1]
-# reset the smallest itemnum (first instance) to 0 because it is not a perseveration
-perseverations[perseveration== 1 & itemnum== min(itemnum), perseveration:=0, by= .(prolific_id,items)]
-# Repeat the perseveration process in main data table so that you can later exclude the values
-dat[, perseveration:= 0]
-# Match the items in the persev and dat by participant and listnum (word i was said x times by participant z on trial y)
-dat[, .N, by= .(prolific_id, listnum, items)][N>1]
+# perseverations= dat
+fwrite(perseverations, 'with_perseverations.csv')
+
+
 # Check that the issue with rows shifting hasn't happened again
   # print(dat[, unique(condition), prolific_id])
 
